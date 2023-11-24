ca17e6529072bfb9bde701f6075ae5668099bf8e
unknown <ozmitel08@gmail.com>
2023-11-24 11:44:28 +0200

    edited V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> from Jenkins
+          Edit <code>src/App.js</code> from Jenkins V2
         </p>
         <a
           className="App-link"
