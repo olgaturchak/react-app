fe59902f78037e0ca79d5e4e6f32ac9e521fda66
unknown <ozmitel08@gmail.com>
2023-11-26 12:51:42 +0200

    ngrok add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> edit!!!
+          Edit <code>src/App.js</code> edit with NGROK!!!
         </p>
         <a
           className="App-link"
