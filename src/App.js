a6b7820dc8c888e13b8820aad61bb86b58a08598
olgaturchak <olgaturchak14@gmail.com>
2023-12-01 10:33:15 +0200

    edit v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> edit by Olga1!
+          Edit <code>src/App.js</code> edit by Olga2!
         </p>
         <a
           className="App-link"
